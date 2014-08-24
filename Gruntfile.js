25179f193794b64dcdf6af3410c921c187e758db
Sergey Gospodarets <sgospodarets@gmail.com>
2014-08-24 19:04:07 +0100

    Grunt serve added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,20 @@
-'use strict';
+/* globals module, require */
+
 module.exports = function (grunt) {
+
+    "use strict";
+
     grunt.initConfig({
-        shell: {
-            jekyllBuild: {
-                command: 'jekyll build'
+        uglify: {
+            global: {
+                files: {
+                    "js/main.min.js": ["_js/main.js"]
+                }
             }
         },
+
         compass: {
-            dist: {
+            global: {
                 options: {
                     sassDir: '_scss',
                     cssDir: 'css',
@@ -17,14 +24,78 @@ module.exports = function (grunt) {
 //                    debugInfo: true
                 }
             }
+        },
+        autoprefixer: {
+            global: {
+                src: "css/style.css",
+                dest: "css/style.css"
+            }
+        },
+
+        shell: {
+            jekyllServe: {
+                command: "jekyll serve",
+                options: {
+                    stdin: false
+                }
+            },
+            jekyllBuild: {
+                command: "jekyll build",
+                options: {
+                    stdin: false
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true
+            },
+            site: {
+                files: ["*.html", "_layouts/*.html", "_posts/*.md", "_includes/*.html"],
+                tasks: ["shell:jekyllBuild"]
+            },
+            js: {
+                files: ["js/*.js"],
+                tasks: ["uglify", "shell:jekyllBuild"]
+            },
+            css: {
+                files: ["scss/**/*.scss"],
+                tasks: ["compass", "autoprefixer", "shell:jekyllBuild"]
+            },
+            svgIcons: {
+                files: ["svg/*.svg"],
+                tasks: ["svgstore", "shell:jekyllBuild"]
+            }
+        },
+
+        svgstore: {
+            options: {
+                prefix: "shape-",
+                cleanup: false,
+                svg: {
+                    style: "display: none;"
+                }
+            },
+            default: {
+                files: {
+                    "_includes/svg-defs.svg": ["svg/*.svg"]
+                }
+            }
         }
 
     });
 
-    require('load-grunt-tasks')(grunt);// alternative for repeating grunt.loadNpmTasks('*');
+    require("load-grunt-tasks")(grunt);
 
-    grunt.registerTask('default', [
-        'compass:dist',
-        'shell:jekyllBuild'
+    grunt.registerTask("serve", ["shell:jekyllServe"]);
+    grunt.registerTask("default", [
+        "uglify",
+        "compass",
+        "autoprefixer",
+        "svgstore",
+        "shell:jekyllBuild",
+        "watch"
     ]);
+
 };
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # [malyw.github.io](http://malyw.github.io/)
+
+1. $ gem install jekyll
+1. $ npm install
+1. $ grunt serve
+1. $ grunt (N.B.: Don't stop previous command)
+1. Open [localhost:4000](http://localhost:4000/)
+1. Enable [LiveReload browser extension](http://feedback.livereload.com/knowledgebase/articles/86242-how-do-i-install-and-use-the-browser-extensions-)
