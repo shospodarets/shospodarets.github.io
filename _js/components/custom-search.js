1512c4d973c0e01580340c2f02e197b4112896e3
Sergey <sgospodarets@gmail.com>
2015-01-08 10:40:52 +0000

    Avoid multiple loading requests for custom search and blur placeholder input before run search

--- _js/components/custom-search.js
+++ _js/components/custom-search.js
@@ -11,6 +11,10 @@ var UTILS = require('../utils/utils'),
 var CustomSearch = function (options) {
     this.options = options;
 
+    this.isLoading = false;
+
+    this._onFocus = this.onFocus.bind(this);// create link function to add/remove it as event listener
+
     this.placeholderForm = document.querySelector('.google-custom-search-placeholder');
     this.placeholderInput = this.placeholderForm.querySelector('input');
     this.originalInput =
@@ -31,6 +35,7 @@ CustomSearch.prototype.defineOriginalElements = function () {
 
 CustomSearch.prototype.runSearch = function (value) {
     this.originalInput.value = value;
+    this.placeholderInput.blur();// remove focus from placeholder input
 
     window.console.log = function () {
     };// to keep console clear from google suggestions
@@ -45,16 +50,27 @@ CustomSearch.prototype.loadSearch = function () {
     );
 };
 
+CustomSearch.prototype.onFocus = function () {
+    if (this.isLoading) return;
+    this.isLoading = true;
+
+    this.loadSearch()
+        .then(function () {
+            // on custom search loaded
+            this.isLoading = false;
+            this.placeholderInput.removeEventListener('focus', this._onFocus);// remove loading request
+
+            this.bindAdditionalEvents();
+        }.bind(this), function () {
+            // on custom search loading error
+            this.isLoading = false;
+        }.bind(this));
+};
+
 // EVENTS
 CustomSearch.prototype.bindEvents = function () {
     // start loading custom search on input focus
-    this.placeholderInput.addEventListener('focus', function () {
-        this.loadSearch()
-            .then(function success() {
-                // on custom search loaded
-                this.bindAdditionalEvents();
-            }.bind(this))
-    }.bind(this));
+    this.placeholderInput.addEventListener('focus', this._onFocus);
 
     // prevent submit of placeholder input
     this.placeholderForm.addEventListener('submit', function (e) {
