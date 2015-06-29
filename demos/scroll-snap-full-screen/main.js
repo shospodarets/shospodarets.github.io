dac0746945688329756ea83a75402dbb53846962
Sergey <sgospodarets@gmail.com>
2015-06-29 21:08:36 +0100

    Scroll snap full page demo

--- demos/scroll-snap-full-screen/main.js
+++ demos/scroll-snap-full-screen/main.js
@@ -4,8 +4,33 @@
 
     if (isScrollSnapSupported) return;
 
-    // USE SCROLLIFY
+
+    /*----- USE SCROLLIFY -----*/
+    var scrollBoxesQuery = '.box';
+    var $boxes = $(scrollBoxesQuery);
+
+    // REMOVE IDs TO PREVENT AN AUTOMATIC BROWSER SCROLLING
+    $boxes.removeAttr('id');
+
+    // SCROLLIFY
     $.scrollify({
-        section: ".box"
+        section: scrollBoxesQuery
+    });
+
+    // SCROLL TO
+    $(".scrollTo").click(function (e) {
+        e.preventDefault();
+        $.scrollify("move", $(this).attr("href"));
+    });
+
+    // SET HEIGHT FOR BROWSERS WITHOUT "height: 100vh;" CSS UNITS SUPPORT
+    $boxes.css({"height": $(window).height()});
+    var timer;
+
+    $(window).resize(function () {
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            $boxes.css({"height": $(window).height()});
+        }, 40);
     });
 }());
