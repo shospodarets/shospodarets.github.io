de6c91ec38db14138dbe3d39ee47e2c30d8d154b
--global <sgospodarets@gmail.com>
2016-01-10 00:01:18 +0000

    Service Worker: Avoid leaking to global scope, message for a user when serving data from cache

--- _js/main/components/register-service-worker.js
+++ _js/main/components/register-service-worker.js
@@ -1,16 +1,17 @@
 /* VARS */
+var humane = require('../libs/humane');// http://wavded.github.io/humane-js/
 
 // enables debug logging in the browser script and Service Worker
-var IS_DEBUG_ENABLED = localStorage.debug || location.search.indexOf('debug') !== -1;
+var isDebugEnabled = localStorage.debug || location.search.indexOf('debug') !== -1;
 
 /* LOGGING */
 function log() {
-    if (!IS_DEBUG_ENABLED) return;
+    if (!isDebugEnabled) return;
     console.log.apply(console, addMessagePrefix('PAGE:', arguments));
 }
 
 function logError() {
-    if (!IS_DEBUG_ENABLED) return;
+    if (!isDebugEnabled) return;
     console.error.apply(console, addMessagePrefix('PAGE:', arguments));
 }
 
@@ -26,7 +27,7 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register(
         swPath
     ).then(function (registration) {
-        sendMessageToWorker({IS_DEBUG_ENABLED: IS_DEBUG_ENABLED});// send debug state info
+        sendMessageToWorker({'---isDebugEnabled---': isDebugEnabled});// send debug state info
         log(`Service Worker "${swPath}" registration successful with scope: ${registration.scope}`);
     }).catch(function (error) {
         // registration failed
@@ -34,7 +35,7 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-/* UTILS */
+/*--- MESSAGING ---*/
 
 // https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/post-message
 function sendMessageToWorker(message) {
@@ -60,5 +61,17 @@ function sendMessageToWorker(message) {
     }).then(()=> {
     }, (err)=> {
         logError(`Message sending to SW failed with error`, err);
+    });
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.addEventListener('message', function (event) {
+        if (event.data['---notification---'] !== undefined) {
+            humane.log(event.data['---notification---'], {
+                addnCls: 'humane-libnotify-info',
+                timeout: 5000,
+                clickToClose: true
+            });
+        }
     });
 }
--- service-worker.js
+++ service-worker.js
@@ -1,121 +1,138 @@
 /* VARS */
 
 // enables debug logging
-var IS_DEBUG_ENABLED = false;
+// in global scope to simplify debugging
+self.isDebugEnabled = false;
 
-const CACHE_NAME = 'v8';
+(()=> {// incapsulate funcs, vars from global scope
+    const CACHE_NAME = 'offline';
 
-/* CACHE REQUIRED ITEMS */
-const urlsToCache = [
-    // html
-    '/',
-    '/index.html',
+    /* CACHE REQUIRED ITEMS */
+    const urlsToCache = [
+        // html
+        '/',
+        '/index.html',
 
-    // css
-    '/css/non-critical.css',
+        // css
+        '/css/non-critical.css',
 
-    // js
-    '/js/main.min.js'
-];
+        // js
+        '/js/main.min.js'
+    ];
 
-self.addEventListener('install', (event) => {
-    //log('SW is installed');
+    self.addEventListener('install', (event) => {
+        log('SW is installed');
+        // OLD SW MIGHT BE STILL ACTIVE HERE
+        event.waitUntil(
+            caches.open(CACHE_NAME)
+                .then((cache) =>cache.addAll(urlsToCache))
+                .catch((err)=> {
+                    logError(`Error opening the cache "${CACHE_NAME}"`, err);
+                })
+        );
+    });
+
+    /* DELETE PREVIOUS CACHES */
+    self.addEventListener('activate', (event) => {
+        log('SW is activated');
+        // CURRENT SW TAKES CONTROL, OLD ISN'T ACTIVE HERE
+        event.waitUntil(
+            caches.keys()
+                .then((cacheNames) => {
+                    return Promise.all(
+                        cacheNames.map((cacheName) => {
+                            if (cacheName !== CACHE_NAME) {
+                                return caches.delete(cacheName);
+                            }
+                        })
+                    );
+                })
+                .catch((err) => {
+                    logError(`Error reading cache keys for "${CACHE_NAME}"`, err);
+                })
+        );
+    });
+
+    /* FETCH */
+    self.addEventListener('fetch', (event) => {
+        // Fetching request as usually if possible updating the cached version
+        // and serve cached version only in cases of errors (offline mode, network or server errors etc.)
+        event.respondWith(
+            caches.match(event.request)
+                .then((responseFromCache) => {
+                        //log('Fetching', event.request.url);
+
+                        // event.request.clone -  as event.request is used after and can be consumed only once
+                        return fetch(event.request.clone())
+                            .then((response) => {
+                                // Check if we received a valid response
+                                if (!response || response.status !== 200 || response.type !== 'basic') {
+                                    return response;
+                                }
+
+                                caches.open(CACHE_NAME)
+                                    .then((cache) => {
+                                        log('Cumulative add to cache', event.request.url);
+
+                                        // response.clone - because response can be consumed only once
+                                        cache.put(event.request, response.clone());
+                                    })
+                                    .catch((err)=> {
+                                        logError(`Error opening the cache "${CACHE_NAME}"`, err);
+                                    });
+
+                                return response;
+                            })
+                            .catch((err)=> {
+                                if (responseFromCache) {
+                                    log(`Serving cached response for "${event.request.url}" because of fetching error`, err);
+                                    sendMessageToPage('Some content was served from browser cache because of connection problems');
+                                    return responseFromCache;
+                                } else {
+                                    logError(`Error fetching the request "${event.request.url}"`, err);
+                                }
+                            });
+                    }
+                )
+                .catch((err)=> {
+                    logError(`Error matching cache for the request "${event.request}"`, err);
+                })
+        );
+    });
+
+    /*--- MESSAGING ---*/
     self.addEventListener('message', (event)=> {
         //log('Handling a message', event);
-        if (event.data.IS_DEBUG_ENABLED !== undefined) {
-            IS_DEBUG_ENABLED = event.data.IS_DEBUG_ENABLED;
+        if (event.data['---isDebugEnabled---'] !== undefined) {
+            isDebugEnabled = event.data['---isDebugEnabled---'];
         }
     });
-    // OLD SW MIGHT BE STILL ACTIVE HERE
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) =>cache.addAll(urlsToCache))
-            .catch((err)=> {
-                logError(`Error opening the cache "${CACHE_NAME}"`, err);
-            })
-    );
-});
 
-/* DELETE PREVIOUS CACHES */
-self.addEventListener('activate', (event) => {
-    //log('SW is activated');
-    // CURRENT SW TAKES CONTROL, OLD ISN'T ACTIVE HERE
-    event.waitUntil(
-        caches.keys()
-            .then((cacheNames) => {
-                return Promise.all(
-                    cacheNames.map((cacheName) => {
-                        if (cacheName !== CACHE_NAME) {
-                            return caches.delete(cacheName);
-                        }
-                    })
-                );
-            })
-            .catch((err) => {
-                logError(`Error reading cache keys for "${CACHE_NAME}"`, err);
-            })
-    );
-});
+    function sendMessageToPage(message) {
+        return self.clients.matchAll()
+            .then(function (clients) {
+                clients.forEach(function (client) {
+                    client.postMessage({
+                        "---notification---": message
+                    });
+                });
+            });
+    }
 
-/* FETCH */
-self.addEventListener('fetch', (event) => {
-    // Fetching request as usually if possible updating the cached version
-    // and serve cached version only in cases of errors (offline mode, network or server errors etc.)
-    event.respondWith(
-        caches.match(event.request)
-            .then((responseFromCache) => {
-                    //log('* [Fetching]: ' + event.request.url);
+    /*--- LOGGING ---*/
+    function log() {
+        if (!isDebugEnabled) return;
+        console.log.apply(console, addMessagePrefix('SW:', arguments));
+    }
 
-                    // event.request.clone -  as event.request is used after and can be consumed only once
-                    return fetch(event.request.clone())
-                        .then((response) => {
-                            // Check if we received a valid response
-                            if (!response || response.status !== 200 || response.type !== 'basic') {
-                                return response;
-                            }
+    function logError() {
+        if (!isDebugEnabled) return;
+        console.error.apply(console, addMessagePrefix('SW:', arguments));
+    }
 
-                            caches.open(CACHE_NAME)
-                                .then((cache) => {
-                                    log('* [Cumulative add to cache:]: ' + event.request.url);
-
-                                    // response.clone - because response can be consumed only once
-                                    cache.put(event.request, response.clone());
-                                })
-                                .catch((err)=> {
-                                    logError(`Error opening the cache "${CACHE_NAME}"`, err);
-                                });
-
-                            return response;
-                        })
-                        .catch((err)=> {
-                            if (responseFromCache) {
-                                console.log(`Serving cached response for "${event.request.url}" because of fetching error`, err);
-                                return responseFromCache;
-                            } else {
-                                logError(`Error fetching the request "${event.request.url}"`, err);
-                            }
-                        });
-                }
-            )
-            .catch((err)=> {
-                logError(`Error matching cache for the request "${event.request}"`, err);
-            })
-    );
-});
-
-/*--- UTILS ---*/
-function log() {
-    if (!IS_DEBUG_ENABLED) return;
-    console.log.apply(console, addMessagePrefix('SW:', arguments));
-}
-
-function logError() {
-    if (!IS_DEBUG_ENABLED) return;
-    console.error.apply(console, addMessagePrefix('SW:', arguments));
-}
-
-function addMessagePrefix(prefix, args) {
-    var res = Array.prototype.slice.apply(args);
-    res.unshift(prefix);
-    return res;
-}
+    function addMessagePrefix(prefix, args) {
+        var res = Array.prototype.slice.apply(args);
+        res.unshift(prefix);
+        return res;
+    }
+})();
