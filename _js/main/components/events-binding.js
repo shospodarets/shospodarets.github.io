1c8a9a15b9eb3e4f7684d38029842dd0b32a5858
--global <shospodarets@gmail.com>
2016-05-28 21:12:06 +0100

    Heading click to update hash

--- _js/main/components/events-binding.js
+++ _js/main/components/events-binding.js
@@ -6,7 +6,7 @@ var EventsBinding = function () {
     this.bindEvents();
 };
 
-// METHODS
+// unload
 EventsBinding.prototype.onUnload = function () {
     var unloadingClass = 'unloading';
 
@@ -17,6 +17,7 @@ EventsBinding.prototype.onUnload = function () {
     }, 5000);
 };
 
+// post click
 EventsBinding.prototype.onPostShortClick = function (e) {
     // Open post content on post-short click
     if (e.target.tagName.toLowerCase() === 'a')  return;// leave links click
@@ -27,16 +28,36 @@ EventsBinding.prototype.onPostShortClick = function (e) {
     }
 };
 
-// BIND EventsBinding
-EventsBinding.prototype.bindEvents = function () {
-    window.addEventListener('beforeunload', this.onUnload);
-    window.addEventListener('unload', this.onUnload);
-
-
+EventsBinding.prototype.bindPostClick = function () {
     var postShorts = document.querySelectorAll('.post-short');
     [].slice.apply(postShorts).forEach(function (postShort) {
         postShort.addEventListener('click', this.onPostShortClick);
     }.bind(this));
+};
+
+// heading click
+EventsBinding.prototype.onHeadingClick = function (e) {
+    // Set window hash
+    var hash = e.target.getAttribute('id');
+    if (hash) {
+        window.location.hash = hash;
+    }
+};
+
+EventsBinding.prototype.bindHeadingClick = function () {
+    var headings = document.querySelectorAll('h1,h2,h3,h4,h5,h6');
+    [].slice.apply(headings).forEach(function (heading) {
+        heading.addEventListener('click', this.onHeadingClick);
+    }.bind(this));
+};
+
+// BIND EVENTS
+EventsBinding.prototype.bindEvents = function () {
+    window.addEventListener('beforeunload', this.onUnload);
+    window.addEventListener('unload', this.onUnload);
+
+    this.bindPostClick();
+    this.bindHeadingClick();
 };
 
 exports.EventsBinding = EventsBinding;
