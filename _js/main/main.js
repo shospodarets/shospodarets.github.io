978f41fe4f7e2087123a553276225950f82a718d
Serg Hospodarets <shospodarets@gmail.com>
2016-10-16 22:18:53 +0100

    Remove unneeded self executing function

--- _js/main/main.js
+++ _js/main/main.js
@@ -5,36 +5,34 @@
  */
 require('babel-polyfill');
 
-(function () {
-    // DEPENDENCIES
-    // components
-    var Events = require('./components/events-binding').EventsBinding;
-    var Analytics = require('./outer_components/analytics').Analytics;
-    // outer components
-    var CustomSearch = require('./outer_components/custom-search').CustomSearch;
-    var ConditionalLoader = require('./outer_components/conditional-loader').ConditionalLoader;
+// DEPENDENCIES
+// components
+const Events = require('./components/events-binding').EventsBinding;
+const Analytics = require('./outer_components/analytics').Analytics;
+// outer components
+const CustomSearch = require('./outer_components/custom-search').CustomSearch;
+const ConditionalLoader = require('./outer_components/conditional-loader').ConditionalLoader;
 
-    require('./components/register-service-worker');// execute file without imports
+require('./components/register-service-worker');// execute file without imports
 
-    document.addEventListener("DOMContentLoaded", function () {
-        // APP
-        var APP = window.jekyllVariables;
-        delete window.jekyllVariables;// cleaning
+document.addEventListener("DOMContentLoaded", function () {
+    // APP
+    const APP = window.jekyllVariables;
+    delete window.jekyllVariables;// cleaning
 
-        // COMPONENTS
-        APP.events = new Events();
-        // OUTER COMPONENTS (loading services/additions from outside)
-        APP.analytics = new Analytics({
-            GOOGLE_ANALYTICS: APP.GOOGLE_ANALYTICS
-        });
-        APP.conditionalLoader = new ConditionalLoader({
-            SITE_BASE_URL: APP.SITE_BASE_URL,
-            DISCUSS_ID: APP.DISCUSS_ID,
-            PAGE_URL: APP.PAGE_URL,
-            PAGE_IDENTIFIER: APP.PAGE_IDENTIFIER
-        });
-        APP.customSearch = new CustomSearch({
-            GOOGLE_SEARCH_ID: APP.GOOGLE_SEARCH_ID
-        });
+    // COMPONENTS
+    APP.events = new Events();
+    // OUTER COMPONENTS (loading services/additions from outside)
+    APP.analytics = new Analytics({
+        GOOGLE_ANALYTICS: APP.GOOGLE_ANALYTICS
     });
-}());
+    APP.conditionalLoader = new ConditionalLoader({
+        SITE_BASE_URL: APP.SITE_BASE_URL,
+        DISCUSS_ID: APP.DISCUSS_ID,
+        PAGE_URL: APP.PAGE_URL,
+        PAGE_IDENTIFIER: APP.PAGE_IDENTIFIER
+    });
+    APP.customSearch = new CustomSearch({
+        GOOGLE_SEARCH_ID: APP.GOOGLE_SEARCH_ID
+    });
+});
