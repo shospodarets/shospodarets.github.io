4d3e2e6c97260765c7b29d0a96a7fc39e6c8a06d
Serg Hospodarets <shospodarets@gmail.com>
2017-04-29 20:43:47 +0100

    CSS Houdini Autochanger and optimization for Generated Backgrounds demo

--- demos/css-colors-from-custom-props/main.js
+++ demos/css-colors-from-custom-props/main.js
@@ -2,8 +2,8 @@ function setProp(prop, value) {
     document.documentElement.style.setProperty('--' + prop, Number(value));
 }
 
-var inputs = document.querySelectorAll('input[type="range"]');
-var inputsArr = Array.prototype.slice.call(inputs);
+const inputs = document.querySelectorAll('input[type="range"]');
+const inputsArr = Array.prototype.slice.call(inputs);
 
 inputsArr.forEach(function (input) {
     input.addEventListener('input', function (e) {
@@ -12,3 +12,58 @@ inputsArr.forEach(function (input) {
 
     setProp(input.name, input.value);// init values
 });
+
+
+// AUTOCHAGER
+const enableAutoChanger = false;
+
+if (enableAutoChanger) {
+    let isValueGrowth = true; //boolean to indicate to add or minus the value
+
+    const firstInput = inputsArr[0];
+
+    setInterval(() => {
+        const value = Number(firstInput.value);
+
+        if (value === 255) {
+            isValueGrowth = false;
+        } else if (value === 0) {
+            isValueGrowth = true;
+        }
+
+        if (isValueGrowth) {
+            firstInput.value = value + 1;
+        } else {
+            firstInput.value = value - 1;
+        }
+
+        // trigger onchange
+        const evt = document.createEvent("HTMLEvents");
+        evt.initEvent("input", false, true);
+        firstInput.dispatchEvent(evt);
+    }, 5);
+}
+
+// OPTIMIZATION
+const enableOptimization = true;
+
+if (enableOptimization && CSS.registerProperty) {
+    // register props
+    CSS.registerProperty({
+        name: '--r',
+        syntax: '<number>',
+        initialValue: 0
+    });
+
+    CSS.registerProperty({
+        name: '--g',
+        syntax: '<number>',
+        initialValue: 0
+    });
+
+    CSS.registerProperty({
+        name: '--b',
+        syntax: '<number>',
+        initialValue: 0
+    });
+}
