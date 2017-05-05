4c9d800ca517077cf69bde9bc39c7607b3f11218
Serg Hospodarets <shospodarets@gmail.com>
2017-05-05 01:24:07 +0100

    Houdini multiple border

---
+++ demos/houdini-multiple-border/main.js
@@ -0,0 +1,30 @@
+// register custom props
+[
+    '--worklet-border-top-color',
+    '--worklet-border-right-color',
+    '--worklet-border-bottom-color',
+    '--worklet-border-left-color',
+].forEach((name) => {
+    CSS.registerProperty({
+        name: name,
+        syntax: '<color>+',
+        inherits: false,
+        initialValue: 'currentcolor',
+    });
+});
+[
+    '--worklet-border-top-width',
+    '--worklet-border-right-width',
+    '--worklet-border-bottom-width',
+    '--worklet-border-left-width',
+].forEach((name) => {
+    CSS.registerProperty({
+        name: name,
+        syntax: '<number>',
+        inherits: false,
+        initialValue: '0',
+    });
+});
+
+// add a paint Worklet
+paintWorklet.addModule('border-colors.js');
