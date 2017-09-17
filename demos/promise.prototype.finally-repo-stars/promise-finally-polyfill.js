4865c5ca46149684e7dc0ae5ca95353b4d115fe0
Serg Hospodarets <shospodarets@gmail.com>
2017-09-17 16:34:08 +0100

    Promise.prototype.finally() demo Added polyfill

---
+++ demos/promise.prototype.finally-repo-stars/promise-finally-polyfill.js
@@ -0,0 +1,28 @@
+(function () {
+    // based on https://github.com/matthew-andrews/Promise.prototype.finally
+
+    // Get a handle on the global object
+    let globalObject;
+    if (typeof global !== 'undefined') globalObject = global;
+    else if (typeof window !== 'undefined' && window.document) globalObject = window;
+
+    // check if the implementation is available
+    if (typeof Promise.prototype['finally'] === 'function') {
+        return;
+    }
+
+    // implementation
+    globalObject.Promise.prototype['finally'] = function finallyPolyfill(callback) {
+        const constructor = this.constructor;
+
+        return this.then(function (value) {
+            return constructor.resolve(callback()).then(function () {
+                return value;
+            });
+        }, function (reason) {
+            return constructor.resolve(callback()).then(function () {
+                throw reason;
+            });
+        });
+    };
+}());
